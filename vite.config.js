68705e5902e4ba65a845d8411829287e3ebb21e5
bishoy labib <bishoylabib78@gmail.com>
2025-04-08 11:01:25 +0200

    Fix margins

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import eslint from "vite-plugin-eslint";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
 });
